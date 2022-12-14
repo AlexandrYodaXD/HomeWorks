e232bc5e4d25025210b7f4d0498b94ec04566347
Alexandr YodaXD <skyfoxalex@gmail.com>
2022-12-14 07:28:07 +0300

    python hw1 changed

--- Python/hw1/Task_2.py
+++ Python/hw1/Task_2.py
@@ -1,11 +1,10 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-# x = input('Введите X: ')
-# y = input('Введите Y: ')
-# z = input('Введите Z: ')
-
-x, y, z = [input(f'Введите {i}: ') for i in ['x', 'y', 'z']]
-
-res = not (x or y or z) == (not x and not y and not z)
-
-print(res)
+for x in [False, True]:
+    for y in [False, True]:
+        for z in [False, True]:
+            if (not (x or y or z)) != (not x and not y and not z):
+                print('Утверждение не верно')
+                break
+else:
+    print('Утверждение верно')
--- Python/hw1/Task_3.py
+++ Python/hw1/Task_3.py
@@ -10,12 +10,12 @@ x = int(input('Введите координату X: '))
 y = int(input('Введите координату Y: '))
 
 if x > 0 and y > 0:
-    print('1')
+    print(1)
 elif x < 0 and y > 0:
-    print('2')
+    print(2)
 elif x < 0 and y < 0:
-    print('3')
+    print(3)
 elif x > 0 and y < 0:
-    print('4')
+    print(4)
 else:
     print('X или Y равны нулю, что не соответствует условию задачи')
