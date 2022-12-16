4ba5591ee9551eb1c2ac2bfa863d3b56cbd36874
Alexandr YodaXD <skyfoxalex@gmail.com>
2022-12-17 01:03:52 +0300

    python hw2 complited

--- Python/hw2/Task_3.py
+++ Python/hw2/Task_3.py
@@ -1,9 +1,6 @@
 # Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, максимум использование библиотеки Random для и получения случайного int
 from random import randint
 
-my_list = [randint(1, 99) for x in range(10)]
-print(f'Изначальный список:  {my_list}')
-
 # берется случайный индекс элемента из левой и правой части списка и меняются местами
 # shuffle_coeff отвечает за колличество перемешиваний (по-идее хватает одного, случайность на то и случайность что элементы могут остаться нетронутыми, либо переместиться дважды (второй раз обратно), но всегда можно накинуть по-больше :) )
 def ListShuffle(inc_list, shuffle_coeff=1):
@@ -14,6 +11,9 @@ def ListShuffle(inc_list, shuffle_coeff=1):
         rnd_1 = randint(0, half_list_len - 1)
         rnd_2 = randint(half_list_len, list_len - 1)
         inc_list[rnd_1],inc_list[rnd_2] = inc_list[rnd_2],inc_list[rnd_1]
+
+my_list = [randint(1, 99) for x in range(10)]
+print(f'Изначальный список:  {my_list}')
 
 ListShuffle(my_list)
 print(f'После перемешивания: {my_list}')
