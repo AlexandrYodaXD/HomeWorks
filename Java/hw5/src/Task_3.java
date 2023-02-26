b468dbf31d13ffd16679b1f398d999ab4013b83d
YodaXD <LyZNF@B@9f42E64>
2023-02-26 21:33:06 +0300

    Java hw5 tasks 1-3 added

---
+++ Java/hw5/src/Task_3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ *Реализовать алгоритм пирамидальной сортировки (HeapSort)
+ */
+public class Task_3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(33, 25, 66, 98, 13, 22, 88));
+    }
+}
