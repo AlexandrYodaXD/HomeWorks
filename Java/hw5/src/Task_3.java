27c1e9b00106655e40cd130e1ef41f28e09b5813
YodaXD <LyZNF@B@9f42E64>
2023-02-28 17:07:53 +0300

    Java hw5 tasks 3 in progress

--- Java/hw5/src/Task_3.java
+++ Java/hw5/src/Task_3.java
@@ -6,31 +6,52 @@ import java.util.Arrays;
  */
 public class Task_3 {
     public static void main(String[] args) {
-//        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(33, 25, 66, 98, 13, 22, 88));
-//        int[] arr = new int[]{33, 25, 88, 98, 13, 22, 66};
         int[] arr = new int[]{5, 7, 9, 4, 3, 8, 3, 1, 0, 3};
-        makeHeap(arr);
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static void makeHeap(int[] arr) {
-        int size = arr.length;
-        ArrayList<Integer> arrLst = new ArrayList<>();
-        arrLst.add(0, 0);
+    static void heapSort(int[] originArray) {
+        int size = originArray.length;
+        ArrayList<Integer> heapList = new ArrayList<>();
+        heapList.add(0, 0);
         for (int i = 1; i < size + 1; i++) {
-            arrLst.add(i, arr[i - 1]);
+            heapList.add(i, originArray[size - i]);
 
             if (i > 1){
                 int kid = i / 2;
-                if (arrLst.get(i) > arrLst.get(kid)){
-                    System.out.println(String.format("До замены: %s", arrLst.toString()));
-                    int temp = arrLst.remove(i);
-                    arrLst.add(i, arrLst.get(kid));
-                    arrLst.remove(kid);
-                    arrLst.add(kid, temp);
-                    System.out.println(String.format("После замены: %s", arrLst.toString()));
+                if (heapList.get(i) < heapList.get(kid)){
+                    System.out.println(String.format("До замены: %s", heapList.toString()));
+                    int temp = heapList.remove(i);
+                    heapList.add(i, heapList.get(kid));
+                    heapList.remove(kid);
+                    heapList.add(kid, temp);
+                    System.out.println(String.format("После замены: %s", heapList.toString()));
                 }
             }
-            System.out.println(arrLst);
         }
+
+        for (int i = 0; i < originArray.length; i++) {
+            rebuildHeap(heapList);
+            originArray[i] = heapList.remove(1);
+        }
+    }
+
+    static void rebuildHeap(ArrayList<Integer> arrayList){
+        for (int i = 1; i < arrayList.size(); i++) {
+
+            if (i > 1){
+                int kid = i / 2;
+                if (arrayList.get(i) < arrayList.get(kid)){
+//                    System.out.println(String.format("До замены: %s", arrayList.toString()));
+                    int temp = arrayList.remove(i);
+                    arrayList.add(i, arrayList.get(kid));
+                    arrayList.remove(kid);
+                    arrayList.add(kid, temp);
+//                    System.out.println(String.format("После замены: %s", arrayList.toString()));
+                }
+            }
+        }
+        System.out.println("Новая куча: " + arrayList);
     }
 }
