48e5ecdd6336b3a82d964c34da7b863406371de8
YodaXD <LyZNF@B@9f42E64>
2023-09-14 23:16:23 +0300

    hw5 complete

--- Python Advanced/hw5/task_3.py
+++ Python Advanced/hw5/task_3.py
@@ -2,13 +2,16 @@
 # абсолютный путь до файла. Функция возвращает кортеж из трёх
 # элементов: путь, имя файла, расширение файла.
 
-def split_file_path(file_path):
-    directory, file_name = file_path.rsplit('/', 1)
-    name, extension = file_name.rsplit('.', 1)
+import os
 
-    return directory, name, '.' + extension
+
+def split_path(file_path):
+    directory, filename = os.path.split(file_path)
+    filename, extension = os.path.splitext(filename)
+
+    return directory, filename, extension
 
 
 file_path = "C:/folder/myfile.txt"
-result = split_file_path(file_path)
+result = split_path(file_path)
 print(result)
